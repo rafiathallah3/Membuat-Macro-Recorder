f2c41f9595725c7a827f58e7a85ad75f45a98c3b
Frisky-R <65345768+Frisky-R@users.noreply.github.com>
2022-01-27 21:00:01 +0700

    a

--- test.py
+++ test.py
@@ -1,28 +1,44 @@
-from pynput import keyboard
+from pynput.keyboard import Key, Controller, KeyCode
+import time
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+keyboard = Controller()
 
-def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+print(KeyCode(char='v').vk)
+# Press and release space
+keyboard.press(KeyCode('<162>'))
+keyboard.press(KeyCode(char='a'))
+time.sleep(1)
+keyboard.release(KeyCode('<162>'))
+keyboard.release(KeyCode(char='a'))
 
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+# Type a lower case A; this will work even if no key on the
+# physical keyboard is labelled 'A'
+
+# from pynput.keyboard import Key, KeyCode, Listener
+
+# def on_press(key):
+#     try:
+#         print(KeyCode(char='<162>').char)
+#         print('alphanumeric key {0} pressed'.format(key.char))
+#     except AttributeError:
+#         print('special key {0} pressed'.format(key))
+#         print(key.value.char)
+
+# def on_release(key):
+#     print('{0} released'.format(key))
+#     if key == Key.esc:
+#         # Stop listener
+#         return False
+
+# # Collect events until released
+# with Listener(
+#         on_press=on_press,
+#         on_release=on_release) as listener:
+#     listener.join()
+
+# # ...or, in a non-blocking fashion:
+# listener = Listener(
+#     on_press=on_press,
+#     on_release=on_release)
+# listener.start()
